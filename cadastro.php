32b7ce2c26ac52e4bb11884fddfa7d13fbcea05a
MicaelSoares <146541151+MicaelSoares7@users.noreply.github.com>
2023-11-18 09:30:07 -0300

    exemplo 6

---
+++ cadastro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <?php
+        $nome = $_POST["nome"];
+        echo "<h2>Bom dia $nome !</h2>";
+    ?>
+</body>
+</html>
